07067d922504295953d17f72a1b7e4f397a832fc
galaparadi <gala_koyin@yahoo.co.id>
2021-07-05 07:46:34 +0700

    server.js - bisa menambahkan ke database

--- server.js
+++ server.js
@@ -1,36 +1,42 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http,{
-  cors:{
+var io = require('socket.io')(http, {
+  cors: {
     origin: "http://localhost",
-    methods: ["GET","POST"]
+    methods: ["GET", "POST"]
   }
 });
+const axios = require('axios');
 
 let port = process.env.PORT || 8080;
 
 io.of(/^\/[\w-]+$/).on('connection', socket => {
-  let {userId, workspace} = socket.handshake.query;
-  const namespace = socket.nsp;
-  socket.on('sending-dochub',(msg) => {
-    console.log(Object.keys(io.sockets.server.engine.clients));
-    socket.broadcast.emit('sending-dochub', msg);
-  });
-  socket.on('ask-all-user-client-id', msg => {
-    socket.broadcast.emit('send-socket-id');
-  })
+  let { username, workspace } = socket.handshake.query;
+
   socket.on('ask-private-chat', (msg) => {
     socket.broadcast.emit('someone-ask-private-chat', msg);
   });
   socket.on('receive-private-chat', msg => {
     msg.destId = socket.id;
-    socket.broadcast.emit('receive-private-chat',msg);
+    socket.broadcast.emit('receive-private-chat', msg);
   });
-  socket.on('join:private',room => {
-    socket.join(room);
-  })
-  socket.on('private-chat',(msg,room)=>{
-    socket.to(room).emit('private-chat', msg);
+  socket.on('private-chat', (data, room) => {
+    axios.post('http://localhost:4000/chats', {
+      partisant: [username, data.receiver],
+      workspace: workspace,
+      chat: {
+        message: data.message,
+        sender: username
+      }
+    })
+    .then(response => {
+      socket.to(room).emit('private-chat', data.message, username);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+    
   })
 });
 
